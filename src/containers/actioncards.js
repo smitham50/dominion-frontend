4395e018fd22ac347aea3052869cabc6a0f61421
Aaron Smith <smitham50@gmail.com>
2019-08-08 11:38:45 -0400

    action cards sorted

--- src/containers/actioncards.js
+++ src/containers/actioncards.js
@@ -4,31 +4,109 @@ import { connect } from 'react-redux'
 
 class ActionCards extends React.Component {
 
-  sortActionCards = () => {
+  getActionCards = () => {
     const actionCards = this.props.cards.filter(card => {
       return card.card_type === "Action"
     })
     this.sortActionCards(actionCards)
   }
 
+  getMoats = () => {
+    const moats = this.props.cards.filter(card => {
+      return card.name === "Moat"
+    })
+    this.renderMoats(moats)
+  }
+
   sortActionCards = (actionCards) => {
-    this.render    
+    console.log("SORTED", actionCards)
+    this.renderCellars(actionCards.filter(card => {
+      return card.name === "Cellar"
+    }))
+    this.getMoats()
+    this.renderWoodCutters(actionCards.filter(card => {
+      return card.name === "Woodcutter"
+    }))
+    this.renderWorkshops(actionCards.filter(card => {
+      return card.name === "Workshop"
+    }))
+    this.renderVillages(actionCards.filter(card => {
+      return card.name === "Village"
+    }))
+    this.renderSmithies(actionCards.filter(card => {
+      return card.name === "Smithy"
+    }))
+    this.renderRemodels(actionCards.filter(card => {
+      return card.name === "Remodel"
+    }))
+    this.renderRemodels(actionCards.filter(card => {
+      return card.name === "Remodel"
+    }))
+    this.renderMilitias(actionCards.filter(card => {
+      return card.name === "Militia"
+    }))
+    this.renderMarkets(actionCards.filter(card => {
+      return card.name === "Market"
+    }))
+    this.renderMines(actionCards.filter(card => {
+      return card.name === "Mine"
+    }))
+     
+  }
+
+  renderCellars = (cellars) => {
+    console.log("CELLARS", cellars)
+  }
+
+  renderMoats = (moats) => {
+    console.log("MOATS", moats)
+  }
+
+  renderWoodCutters = (woodcutters) => {
+    console.log("WOODCUTTERS", woodcutters)
+  }
+
+  renderWorkshops = (workshops) => {
+    console.log("WORKSHOPS", workshops)
+  }
+
+  renderVillages = (villages) => {
+    console.log("VILLAGES", villages)
+  }
+
+  renderSmithies = (smithies) => {
+    console.log("SMITHIES", smithies)
+  }
+
+  renderRemodels = (remodels) => {
+    console.log("REMODELS", remodels)
+  }
+
+  renderMilitias = (militias) => {
+    console.log("MILITIAS", militias)
+  }
+
+  renderMarkets = (markets) => {
+    console.log("MARKETS", markets)
+  }
+
+  renderMines = (mines) => {
+    console.log("MINES", mines)
   }
 
 
-
   render() {
-    console.log("RENDER", this.props.cards)
     return(
       <div id="action-card-area" >
-        {this.sortActionCards()}
+        {
+        this.getActionCards()
+        }
       </div>
     )
   }
 }
 
 function msp(state) {
-  console.log("ACTION CONTAINER", state)
 
   return {
     cards: state.game.cards
