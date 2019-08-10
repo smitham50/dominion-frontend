d51216dc774e473023a149319a880dc8348cf663
Aaron Smith <smitham50@gmail.com>
2019-08-10 14:46:01 -0400

    hand discard at end of turn works

--- src/containers/playerdiscard.js
+++ src/containers/playerdiscard.js
@@ -2,10 +2,17 @@ import React from 'react'
 import Card from '../components/card'
 
 class PlayerDiscard extends React.Component {
+
+  renderDiscard = () => {
+    return this.props.discard.map((card, index) => {
+      return <Card key={card.id} card={card} index={index} className="discard-card" />
+    })
+  }
+
   render() {
     return (
       <div className="player-discard" >
-        {}
+        {this.props.discard.length > 0 ? this.renderDiscard() : null}
       </div>
     )
   }
