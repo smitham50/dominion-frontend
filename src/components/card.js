b225a140bd5bac7a152ed4eeadc919d3f2cbb8c1
Aaron Smith <smitham50@gmail.com>
2019-08-17 10:04:46 -0400

    hand doesn't flip back over until 2 cards discarded for militia

--- src/components/card.js
+++ src/components/card.js
@@ -253,14 +253,14 @@ class Card extends React.Component {
   }
 
   render() {
-    const { index, id, card, hand1, hand2, player, playerTurn, className, militia } = this.props
+    const { index, id, card, hand1, hand2, player, playerTurn, className, militia, militiaDiscardSecond } = this.props
     return(
       <Fragment>
         {
           index === 0 
           ?
           <img src={
-              id === "deck-card" || ((className === "hand-card" && playerTurn === false && player === "player2" && !militia) || (className === "hand-card" && playerTurn === true && player === "player1" && !militia) ) 
+              id === "deck-card" || ((className === "hand-card" && playerTurn === false && player === "player2" && !militia && !militiaDiscardSecond) || (className === "hand-card" && playerTurn === true && player === "player1" && !militia && !militiaDiscardSecond) ) 
             ? 
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnxjXZTHcRqwUrA4nW09UvtRlXPGlhAZdOQC6_-s71LayIknwS" 
             : 
@@ -268,7 +268,7 @@ class Card extends React.Component {
           </img> 
           : 
           <img src={
-              id === "deck-card" || ((className === "hand-card" && playerTurn === false && player === "player2" && !militia) || (className === "hand-card" && playerTurn === true && player === "player1" && !militia)) 
+              id === "deck-card" || ((className === "hand-card" && playerTurn === false && player === "player2" && !militia && !militiaDiscardSecond) || (className === "hand-card" && playerTurn === true && player === "player1" && !militia && !militiaDiscardSecond)) 
             ? 
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnxjXZTHcRqwUrA4nW09UvtRlXPGlhAZdOQC6_-s71LayIknwS" 
             :
